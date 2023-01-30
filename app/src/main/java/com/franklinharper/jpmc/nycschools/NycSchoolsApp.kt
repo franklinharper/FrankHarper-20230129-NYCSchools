68992751f01135924b0da6f49f1dd822abd1fac6
Frank Harper <frank@franklinharper.com>
2023-01-30 13:57:00 -0800

    Add Timber for production logging

---
+++ app/src/main/java/com/franklinharper/jpmc/nycschools/NycSchoolsApp.kt
@@ -0,0 +1,39 @@
+package com.franklinharper.jpmc.nycschools
+
+import android.app.Application
+import android.util.Log
+import timber.log.Timber
+import timber.log.Timber.*
+
+
+class NycSchoolsApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        } else {
+            Timber.plant(CrashReportingTree())
+        }
+    }
+
+    /** A tree which logs important information for crash reporting.  */
+    private class CrashReportingTree : Tree() {
+        override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+            if (priority == Log.VERBOSE || priority == Log.DEBUG) {
+                return
+            }
+            // TODO implement crash reporting in production
+// Example code below is from Timber github.
+//            FakeCrashLibrary.log(priority, tag, message)
+//            if (t != null) {
+//                if (priority == Log.ERROR) {
+//                    FakeCrashLibrary.logError(t)
+//                } else if (priority == Log.WARN) {
+//                    FakeCrashLibrary.logWarning(t)
+//                }
+//            }
+        }
+    }
+
+}
